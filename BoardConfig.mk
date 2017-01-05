b6e36abb921618bf2ff7a36614547599149fb90f
YumeMichi <do4suki@gmail.com>
2017-01-05 21:22:16 +0800

    onyx: Remove CM parts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,8 +81,7 @@ TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# CM Hardware
-BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
+# Android Native DT2W
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Enable dexpreopt to speed boot time
