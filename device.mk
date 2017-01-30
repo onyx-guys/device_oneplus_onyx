989c49eb31fa49acad80b689dddd01ae5070aef3
Eino-Ville Talvala <etalvala@google.com>
2017-01-29 18:22:44 -0800

    onyx: Disable camera treble path
    Test: Compiles, property in build.prop as expected
    Bug: 34786432
    Change-Id: Ia09fdaf3a38d07536b0f5ec97770169ce6d7f660

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
     libshims_atomic \
     Snap
 
+# Disable camera Treble path
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_treble=true
+
 # Camera HIDL interfaces
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
