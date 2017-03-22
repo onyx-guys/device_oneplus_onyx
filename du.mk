cc27fdb5f3fff65e753db16e908c578a06e3bae7
YumeMichi <do4suki@gmail.com>
2017-03-22 10:28:48 +0800

    onyx: Add DU build type flag
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- du.mk
+++ du.mk
@@ -24,6 +24,9 @@ $(call inherit-product, vendor/du/config/common_full_phone.mk)
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
+# UNOFFICIAL build tag
+DU_BUILD_TYPE := YumeMichi
+
 PRODUCT_NAME := du_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
