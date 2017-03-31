e789b171b8a03640f4450d1b1f094bba64440699
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    bluetooth: Add libbt-vendor to products
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- device.mk
+++ device.mk
@@ -116,7 +116,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    libbt-vendor
 
 # Graphics
 PRODUCT_PACKAGES += \
