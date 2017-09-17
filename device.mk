42133d4f6c9bb2605461a8ed36c97413196675d3
David <Davidteri91@gmail.com>
2017-09-17 18:59:18 +0800

    onyx: Add configstore HAL interfaces package needed by surfaceflinger and VSYNC read: https://android.googlesource.com/platform/hardware/interfaces/+/200825e359f7481f0916c61f1d55a718a39f457b
    Conflicts:
    	device.mk
    
    Change-Id: Ida07ff3ee4de4eafa577b739ee305c2b98289375

--- device.mk
+++ device.mk
@@ -295,6 +295,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Configstore HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
