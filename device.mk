fdfef6eea1f2f0d0bd4905934b0f92192c8c64a2
YumeMichi <do4suki@gmail.com>
2017-09-24 17:17:24 +0000

    onyx: liblight: Turn into a binderized HAL * Taken from libra, adapted to onyx
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -212,13 +212,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
-# Lights
-PRODUCT_PACKAGES += \
-    lights.msm8974
-
 # Lights HIDL interfaces
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl
+    android.hardware.light@2.0-service.onyx
 
 # Vibrator HIDL interfaces
 PRODUCT_PACKAGES += \
