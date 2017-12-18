69e54df5a2cb13ffa240859ed04a74b713403b20
yuv <dailytroll92@gmail.com>
2017-12-17 18:38:07 -0700

    different anmes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo XOS_onyx-user
-add_lunch_combo XOS_onyx-userdebug
-add_lunch_combo XOS_onyx-eng
+add_lunch_combo yuv_onyx-user
+add_lunch_combo yuv_onyx-userdebug
+add_lunch_combo yuv_onyx-eng
